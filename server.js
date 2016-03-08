229ee6f70cf5992ec2d7f38ee01044a9693ecbba
John Bicknell <john@geckotec.co.uk>
2016-03-08 15:34:36 +0000

    Removed hotloading gumpf from config

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ var config = require('./webpack.config');
 
 new WebpackDevServer(webpack(config), {
   index:'index.html',
-  contentBase:'./app/',
-  hot: true,
-  historyApiFallback: true
+  contentBase:'./app/'
 }).listen(3000, '0.0.0.0', function (err, result) {
   if (err) {
     return console.log(err);
